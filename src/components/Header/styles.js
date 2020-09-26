1ba777968524e2c4ec0cff60243ef5bc0e050ac2
Sanjaydotcom <nssanjay7@gmail.com>
2020-09-26 20:32:02 +0530

    test

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,27 +1,29 @@
 import { makeStyles } from '@material-ui/core/styles';
-const primary = "#324466";
+const primary = "#324466"; 
 
 export default makeStyles(theme => ({
   appBar: {
     position: 'relative',
-    backgrounColor: primary
+    backgrounColor:primary
   },
   roots: {
     width: 'auto',
-    backgroundColor: primary,
+      backgroundColor: primary,
     color: theme.palette.text.primary,
-    height: "60px!important",
-    paddingTop: "5px",
+    height:"60px!important",
+    paddingTop:"5px",
+    textAlign:"center",
+
   },
   stepper: {
-    width: '950px',
-    height: "60px",
-
-    marginLeft: "theme.spacing(2)",
+    width: 'auto',
+    height:"60px",
+   
+      marginLeft: "theme.spacing(2)",
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing() * 2)]: {
-      width: "1000",
-
+      width: "600",
+    
       marginLeft: '25%',
       marginRight: '25%'
     },
