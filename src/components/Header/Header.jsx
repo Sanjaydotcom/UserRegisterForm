fa74b16b7af9bbf74ff5532c1c4ba919d72c9391
Sanjaydotcom <nssanjay7@gmail.com>
2020-09-30 20:39:15 +0530

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header(props) {
     <div className={classes.roots}>
       <div position="absolute" className={classes.appBar}>
         <Grid container spacing={2} className={classes.stepper} >
-        <Grid item xs={0} lg={4} md={4} sm={0} justify="center" >
+        <Grid item xs={0} lg={4} md={4} sm={0} >
         </Grid>
 
           <Grid item xs={12} lg={8} md={8} sm={12} justify="center" >
